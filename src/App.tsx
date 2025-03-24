3068922d00317aaa7ca14cab8a6f190e3a17a35e
gyeol-mi <gyeolmi9876@gmail.com>
2025-03-24 21:50:42 +0900

    #3.6_Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 function App() {
-  const [value, setValue] = useState("");
-  //////////////////////////////event의 타입
-  //////////1. 이벤트의 타입: formEvent
-  //////////2. 어떤 요소에서 발생? formElement에서 발생
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    // console.log(event.currentTarget.value)
-    // 다음 코드는 js의 구조 분해 할당을 사용한 코드
-    // const value = event.currentTarget.value; 와 같은 역할을 함
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-      </form>
+      <Container>
+        <H1>Protected</H1>
+      </Container>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./theme.ts";
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+  document.getElementById("root")
 );
